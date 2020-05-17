e09e86ce0f60288f6bbb88e4b34da25919fd13f0
Prabhatee <Prabhatee@DELL>
2020-05-17 19:17:17 +0530

    comment added

--- HelloWorldBoot/src/main/java/com/example/demo1/HelloWorldBootApplication.java
+++ HelloWorldBoot/src/main/java/com/example/demo1/HelloWorldBootApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorldBootApplication {
 
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldBootApplication.class, args);
 		System.out.println("hi test");
